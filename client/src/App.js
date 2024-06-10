e5f6cdcf5499eee01cb8ea61059a03c2c744b5e9
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-06-10 09:45:24 +0530

    setting up deployed contract in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
 import "./App.css";
 import { connect, disconnect } from "@argent/get-starknet";
 import { useState } from "react";
-import { Contract } from "starknet";
+import { Contract, RpcProvider } from "starknet";
+import contractABI from "./abis/cryptocash_starknet_Cryptocash.contract_class.json";
 function App() {
   const [account, setAccount] = useState(null);
+  const Contract_Address =
+    "0x060942c0b6e4bec865e845eb5f952bceb6ce07ab870c4b0728edc7e0dbaa728b";
   async function connectWallet() {
     const connection = await connect();
     if (connection && connection.isConnected) {
       setAccount(connection.account);
     }
   }
+  async function createNote() {
+    const contract = new Contract(contractABI, Contract_Address, account);
+  }
+  async function get_owner() {
+    const provider = new RpcProvider();
+    const contract = new Contract(contractABI, Contract_Address, provider);
+    const owner = await contract.get_owner();
+    console.log(owner);
+  }
   return (
     <div>
       <button onClick={connectWallet}>Connect</button>
+      <button onClick={get_owner}>get_owner</button>
     </div>
   );
 }
