3d92defcc8712366c8ccb0fed92679eb388b3851
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-06-12 22:32:05 +0530

    added new abi

--- client/src/createabi.mjs
+++ client/src/createabi.mjs
@@ -4,7 +4,7 @@ const provider = new RpcProvider({
   nodeUrl: "https://starknet-sepolia.public.blastapi.io/rpc/v0_7",
 });
 const addrContract =
-  "0x060942c0b6e4bec865e845eb5f952bceb6ce07ab870c4b0728edc7e0dbaa728b";
+  "0x003300c4a4bba7ff2362fc5e44e5cb8e477577f51bde4b96a3628fb20f5f885c";
 const compressedContract = await provider.getClassAt(addrContract);
 writeFileSync(
   "./myAbi.json",
