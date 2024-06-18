fb7410eea482fed37325dd342d3448177ac9d332
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-06-18 13:04:07 +0530

    adding layer 1 layer 2 messaging

--- contract/main.sol
+++ contract/main.sol
@@ -4,6 +4,7 @@ import "./verifier.sol";
 import "./starknet/IStarknetMessaging.sol" ; 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 contract Main is ReentrancyGuard{ 
+  // address for starknet core - 0xE2Bb56ee936fd6433DC0F6e7e3b8365C906AA057
     address payable public _owner;
     IStarknetMessaging public  _snMessaging;  
     Groth16Verifier instance; //stores the instance of deployed verifier contract on chain.
@@ -14,25 +15,21 @@ contract Main is ReentrancyGuard{
         _owner=payable(msg.sender); //owner is contract creator
     }
 
-    function verify(uint256[2] calldata _pA, uint256[2][2] calldata _pB, uint256[2] calldata _pC, bytes32 _nullifierHash, bytes32 _commitment, address _recipient)view public returns(bool) { 
+    function verify(uint256[2] calldata _pA, uint256[2][2] calldata _pB, uint256[2] calldata _pC, bytes32 _nullifierHash, bytes32 _commitment, address _recipient,uint256 contractAddress,
+        uint256 selector)external payable{ 
     bool success=instance.verifyProof(_pA, _pB, _pC, [uint256(_nullifierHash),uint256(_commitment),uint256(uint160(_recipient))]);
     require(success,"Invalid Proof");    
-    return success;
-  }
-   function sendMessageValue(
-        uint256 contractAddress,
-        uint256 selector
-    )
-        external
-        payable
-    {
-        uint256[] memory payload = new uint256[](1);
-        payload[0] = 1 ;
+       uint256[] memory payload = new uint256[](4);
+        payload[0] = uint128(uint256(_nullifierHash)) ;
+        payload[1] = uint128(uint256(_nullifierHash) >>128) ; 
+        payload[2] = uint128(uint256(_commitment));
+        payload[3] = uint128(uint256(_commitment)>>128 );
 
-        _snMessaging.sendMessageToL2{value: msg.value}(
+         _snMessaging.sendMessageToL2{value: msg.value}(
             contractAddress,
             selector,
-            payload
+            payload 
         );
     }
 }
+   
