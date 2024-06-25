d3e669066838c06c80b1358fa420dc70f29b7edd
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-06-25 14:58:55 +0530

    shifted to typescript

--- client/src/utils/createHash.js
+++
@@ -1,9 +0,0 @@
-import { poseidon } from "circomlibjs";
-import bigInt from "big-integer";
-export const nullifierHash = (nullifier) => {
-  return poseidon([bigInt(nullifier)]);
-};
-
-export const commitmentHash = (nullifier, secret) => {
-  return poseidon([bigInt(nullifier), bigInt(secret)]);
-};
