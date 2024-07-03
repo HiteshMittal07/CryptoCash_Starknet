526e16104b6945b60654445bddb9ef2fea496f7c
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-07-03 23:10:27 +0530

    bugs fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,15 @@ import { connect } from "@argent/get-starknet";
 import { Contract, RpcProvider } from "starknet";
 import contract_token_ABI from "./ABI/ETHAbi.json";
 import { commitmentHash } from "./utils/createHash";
+import contractABI from "./ABI/myAbi.json";
 function App() {
   const [account, setAccount] = useState();
-  const STRK_token_address = "";
-  const Contract_Address = "";
+  const STRK_token_address =
+    "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d";
+  const ETH_token_address =
+    "0x049D36570D4e46f48e99674bd3fcc84644DdD6b96F7C741B1562B82f9e004dC7";
+  const Contract_Address =
+    "0x062816a8d6e835b79f1e1962bf30976dc2957c2e126b22beb970419180d502f2";
   async function connectWallet() {
     const connection = await connect({ dappName: "CryptoCash" });
     if (connection && connection.isConnected) {
@@ -41,10 +46,7 @@ function App() {
     const secret = rbigint();
     console.log(secret);
     const nullifier = rbigint();
-    const commitment_hash = commitmentHash(
-      parseInt(nullifier),
-      parseInt(secret)
-    );
+    const commitment_hash = commitmentHash(nullifier, secret);
     console.log(commitment_hash);
     const hex_commitment_hash = toHex(commitment_hash);
     console.log(hex_commitment_hash);
@@ -69,7 +71,11 @@ function App() {
     const hexString = bigIntNumber.toString(16);
     return "0x" + hexString;
   }
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <button onClick={createNote}>create</button>
+    </div>
+  );
 }
 
 export default App;
