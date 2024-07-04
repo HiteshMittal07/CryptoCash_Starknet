12fb7d55fe97162b67262d3c4820bda7d98c2c4b
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-07-04 13:47:50 +0530

    intialised component section

---
+++ client/src/components/createNote.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { approve, getContract, getProvider, toHex } from "../web3/web3";
+import { rbigint } from "../utils/random";
+import { commitmentHash } from "../utils/createHash";
+
+export default function CreateNote(props: any) {
+  async function createNote() {
+    const account = props.account;
+    const contract = getContract(account);
+    const provider = getProvider();
+    const inputElement = document.querySelector("#amount") as HTMLInputElement;
+    const amount = inputElement.value.toString();
+    await approve(amount, account);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    const secret = rbigint();
+    console.log(secret);
+    const nullifier = rbigint();
+    console.log(nullifier);
+    const commitment_hash = commitmentHash(nullifier, secret);
+    console.log(commitment_hash);
+    const hex_commitment_hash = toHex(commitment_hash);
+    console.log(hex_commitment_hash);
+    try {
+      const tx = await contract.createNote(hex_commitment_hash, amount);
+      const transactionHash = tx.transaction_hash;
+      console.log(transactionHash);
+      const txReceipt = await provider.waitForTransaction(transactionHash);
+      const listEvents = txReceipt.events;
+      console.log(listEvents[2].keys[1]);
+      console.log(parseInt(listEvents[2].data[0], 16));
+    } catch (error) {
+      alert(error);
+    }
+  }
+  return (
+    <div>
+      <input type="text" id="amount" placeholder="enter the amount" />
+      <button onClick={createNote}>Create</button>
+    </div>
+  );
+}
