5cd2e4183ebb1d4c3d2fbac92e83d6caa1fdb736
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-07-11 14:21:00 +0530

    added dyanamic api for seamless connection

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { StarknetWalletConnectors } from "@dynamic-labs/starknet";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <DynamicContextProvider
+    settings={{
+      environmentId: "23cee492-ceb0-4f38-8bbe-986290e4dd80",
+      walletConnectors: [StarknetWalletConnectors],
+      initialAuthenticationMode: "connect-only",
+      events: {
+        onAuthSuccess: () => {
+          toast.success("connected");
+        },
+      },
+    }}
+  >
+    <BrowserRouter>
+      <App />
+      <ToastContainer />
+    </BrowserRouter>
+  </DynamicContextProvider>
 );
