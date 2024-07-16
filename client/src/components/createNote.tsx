91e16cc4b566a8eb18a4202bd1c536d0fcd7e067
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-07-16 15:15:08 +0530

    added proper downloading of notes

--- client/src/components/createNote.tsx
+++ client/src/components/createNote.tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import { approve, getContract, getProvider } from "../web3/web3";
-import { rbigint } from "../utils/random";
-import { commitmentHash } from "../utils/createHash";
+import React, { useContext } from "react";
+import {
+  approve,
+  get_token_name,
+  getContract,
+  getProvider,
+} from "../web3/web3";
+import { random } from "../utils/random";
+import { commitmentHash, nullifierHash } from "../utils/createHash";
+import { Button } from "flowbite-react";
+import { downloadQRCodePDF } from "../utils/downloadNote";
+import { CreateNoteQR } from "../utils/createNoteQR";
+import { CryptoContext } from "../context/cryptoContext";
 
-export default function CreateNote(props: any) {
+export default function CreateNote() {
+  const { account } = useContext(CryptoContext);
   async function createNote() {
-    const account = props.account;
     const contract = getContract(account);
     const provider = getProvider();
     const inputElement = document.querySelector("#amount") as HTMLInputElement;
@@ -15,30 +24,32 @@ export default function CreateNote(props: any) {
     const address = inputElement2.value.toString();
     const amount = inputElement.value.toString();
     await approve(amount, account, address);
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    const secret = rbigint();
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    const secret = random();
     console.log(secret);
-    const nullifier = rbigint();
+    const nullifier = random();
     console.log(nullifier);
     const commitment_hash = commitmentHash(nullifier, secret);
     console.log(commitment_hash);
     try {
       const tx = await contract.createNote(commitment_hash, amount, address);
-      // const transactionHash = tx.transaction_hash;
-      // console.log(transactionHash);
-      // const txReceipt = await provider.waitForTransaction(transactionHash);
-      // const listEvents = txReceipt.events;
-      // console.log(listEvents[2].keys[1]);
-      // console.log(parseInt(listEvents[2].data[0], 16));
+      const transactionHash = tx.transaction_hash;
+      console.log(transactionHash);
+      const txReceipt = await provider.waitForTransaction(transactionHash);
+      console.log("🚀 ~ createNote ~ txReceipt:", txReceipt);
+      const note_string = `${secret},${nullifier},${nullifierHash},${commitment_hash},${amount},${address}`;
+      const qrDataURL = await CreateNoteQR(note_string);
+      const token_name = get_token_name(address);
+      downloadQRCodePDF(qrDataURL, amount, token_name);
     } catch (error) {
       alert(error);
     }
   }
   return (
-    <div>
+    <div className="flex flex-col m-20">
       <input type="text" id="amount" placeholder="enter the amount" />
       <input type="text" id="address" placeholder="enter token address" />
-      <button onClick={createNote}>Create</button>
+      <Button onClick={createNote}>Create</Button>
     </div>
   );
 }
--- client/src/utils/downloadNote.ts
+++ client/src/utils/downloadNote.ts
@@ -1,4 +1,3 @@
-import { CreateNoteQR } from "./createNoteQR";
 import jsPDF from "jspdf";
 export function downloadQRCodePDF(
   qrDataURL: any,
@@ -7,7 +6,7 @@ export function downloadQRCodePDF(
 ) {
   const pdf = new jsPDF();
   pdf.setFontSize(30);
-  pdf.text("Crpto Cash Powered by starknet", 65, 14);
+  pdf.text("Crypto Cash", 65, 14);
   pdf.addImage(qrDataURL, "JPEG", 45, 20, 100, 100);
   pdf.setTextColor("#808080");
   pdf.text("https://cryptocash_starknet.netlify.app", 25, 140);
@@ -15,5 +14,5 @@ export function downloadQRCodePDF(
   pdf.text("Use the QR Code to withdraw the value.", 25, 130);
   pdf.text(`Value:${denomination} ${tokenName}`, 25, 150);
   pdf.text(`Network:Starknet`, 25, 160);
-  pdf.save(`CrptoCash${denomination}ETH.pdf`);
+  pdf.save(`CryptoCash${denomination}ETH.pdf`);
 }
